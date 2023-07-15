e4f068b042e1c8c179e5dcf1797df8d7a1acfc92
avagadro <p-avagadro@yandex.com>
2023-07-16 00:37:27 +0300

    Switch WSGI to eventlet

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
+import eventlet
+from eventlet import wsgi
 from api import app, HOST, PORT
-from gevent.pywsgi import WSGIServer
 
 
 if __name__ == '__main__':
-    server = WSGIServer((HOST, PORT), app, environ={'wsgi.multithread': True})
-    server.serve_forever()
+    wsgi.server(eventlet.listen((HOST, PORT)), app)
